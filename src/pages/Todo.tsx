004c51420f575b6fdf67e0e20c39a778511ec617
jonghun <minn4072@gmail.com>
2023-04-06 06:50:59 +0900

    feat: 수정, 삭제 리스트 보여주기 구현

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -5,10 +5,13 @@ import postCreateTodo from "../services/postCreateTodo";
 import { TodoType } from "../types/todo";
 import hasToken from "../utils/hasToken";
 import deleteTodo from "../services/deleteTodo";
+import putTodo from "../services/putTodo";
 
 function Todo() {
   const [todoText, setTodoText] = useState("");
   const [todos, setTodos] = useState<TodoType[]>([]);
+  const [toggleId, setToggleId] = useState<number>(0);
+  const [editTodoText, setEditTodoText] = useState("");
 
   const navigate = useNavigate();
 
@@ -24,11 +27,35 @@ function Todo() {
     setTodoText("");
   };
 
+  const onChangeEditTodoText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditTodoText(e.target.value);
+  };
+
   const delTodo = async (id: number) => {
-    await deleteTodo(id);
-    getTodos().then((res) => {
-      setTodos([...res]);
-    });
+    const copy = todos.filter((todo) => todo.id !== id);
+    deleteTodo(id);
+    setTodos([...copy]);
+  };
+
+  const onChangeCheckBox = (id: number) => {
+    const copy = todos.map((todo) =>
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    );
+
+    const data = copy.find((todo) => todo.id === id);
+    const todoInfo = { todo: data?.todo, isCompleted: data?.isCompleted };
+    putTodo(id, todoInfo);
+    setTodos([...copy]);
+  };
+
+  const updateTodos = (id: number) => {
+    const copy = todos.map((todo) =>
+      todo.id === id ? { ...todo, todo: editTodoText } : todo
+    );
+    const data = copy.find((todo) => todo.id === id);
+    const todoInfo = { todo: editTodoText, isCompleted: data?.isCompleted };
+    putTodo(id, todoInfo);
+    setTodos([...copy]);
   };
 
   useEffect(() => {
@@ -52,22 +79,67 @@ function Todo() {
         </button>
       </div>
       <ul>
-        {todos?.map((todo) => (
-          <li key={todo.id}>
-            <label>
-              <input type="checkbox" defaultChecked={todo.isCompleted} />
-              <span>{todo.todo}</span>
-              <button>수정</button>
-              <button
-                onClick={() => {
-                  delTodo(todo.id);
-                }}
-              >
-                삭제
-              </button>
-            </label>
-          </li>
-        ))}
+        {todos?.map(({ id, todo, isCompleted }) => {
+          return (
+            <li key={id}>
+              <label>
+                <input
+                  type="checkbox"
+                  onClick={() => {
+                    onChangeCheckBox(id);
+                  }}
+                  defaultChecked={isCompleted}
+                />
+                {toggleId !== id ? (
+                  <>
+                    <span>{todo}</span>
+                    <button
+                      onClick={() => {
+                        setEditTodoText(todo);
+                        setToggleId(id);
+                      }}
+                    >
+                      수정
+                    </button>
+                    <button
+                      onClick={() => {
+                        delTodo(id);
+                      }}
+                    >
+                      삭제
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <input
+                      data-testid="modify-input"
+                      type="text"
+                      value={editTodoText}
+                      onChange={onChangeEditTodoText}
+                    />
+                    <button
+                      data-testid="submit-button"
+                      onClick={() => {
+                        updateTodos(id);
+                        setToggleId(0);
+                      }}
+                    >
+                      제출
+                    </button>
+                    <button
+                      data-testid="cancel-button"
+                      onClick={() => {
+                        setToggleId(0);
+                      }}
+                    >
+                      취소
+                    </button>
+                  </>
+                )}
+              </label>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
