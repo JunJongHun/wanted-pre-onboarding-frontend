1af45f00aff244fd4c17bb9c8fac5b8b8c407e00
jonghun <minn4072@gmail.com>
2023-04-06 00:44:17 +0900

    refactor: 중복 메소드 리펙토링

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -8,20 +8,15 @@ function SignUp() {
   //상태값
   const [info, setInfo] = useState<InfoType>({ email: "", password: "" });
   // 유효성 검사
-  const [isEmail, setIsEmail] = useValidate(info.email, /.*@.*/);
-  const [isPassword, setIsPassword] = useValidate(info.password, /.{8,}/);
+  const [isEmail] = useValidate(info.email, /.*@.*/);
+  const [isPassword] = useValidate(info.password, /.{8,}/);
 
   //라우팅
   const navigate = useNavigate();
 
-  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const email = e.target.value;
-    setInfo((pre) => ({ ...pre, email }));
-  };
-
-  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const password = e.target.value;
-    setInfo((pre) => ({ ...pre, password }));
+  const onChangeInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInfo((pre) => ({ ...pre, [name]: value }));
   };
 
   const onSubmit = async () => {
@@ -44,20 +39,23 @@ function SignUp() {
     <div>
       <div>
         <input
-          data-testid="email-input"
+          name="email"
           placeholder="email"
+          data-testid="email-input"
+          type="email"
           value={info.email}
-          onChange={onChangeEmail}
+          onChange={onChangeInfo}
         />
         {!isEmail ? <span>이메일 형식 X</span> : undefined}
       </div>
       <div>
         <input
-          data-testid="password-input"
+          name="password"
           placeholder="password"
+          data-testid="password-input"
           type="password"
           value={info.password}
-          onChange={onChangePassword}
+          onChange={onChangeInfo}
         />
         {!isPassword ? <span>비밀번호 8 자리 이상</span> : undefined}
       </div>
