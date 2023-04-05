114c4e7ed7fdeb0a80300f6ae6311c403d0460d5
jonghun <minn4072@gmail.com>
2023-04-05 21:19:37 +0900

    refactor: state 관리 및 코드 리펙토링

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,55 +1,45 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import postSignUp from "../services/postSignUp";
+import { InfoType } from "../types/info";
 function SignUp() {
   //상태값
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
+  const [info, setInfo] = useState<InfoType>({ email: "", password: "" });
   // 유효성 검사
   const [isEmail, setIsEmail] = useState(false);
   const [isPassword, setIsPassword] = useState(false);
-
+  console.log(isEmail, isPassword);
   //라우팅
   const navigate = useNavigate();
 
   const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     const emailRegex = /.*@.*/;
-    const emailCurrent = e.target.value;
-    setEmail(emailCurrent);
+    const email = e.target.value;
 
-    if (!emailRegex.test(emailCurrent)) {
-      setIsEmail(false);
-    } else {
-      setIsEmail(true);
-    }
+    setInfo((pre) => ({ ...pre, email }));
+    setIsEmail(emailRegex.test(email));
   };
 
   const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const passwordCurrent = e.target.value;
-    setPassword(passwordCurrent);
+    const passwordRegex = /.{8,}/;
+    const password = e.target.value;
 
-    if (password.length <= 8) {
-      setIsPassword(false);
-    } else {
-      setIsPassword(true);
-    }
+    setInfo((pre) => ({ ...pre, password }));
+    setIsPassword(passwordRegex.test(password));
   };
 
   const onSubmit = async () => {
-    alert("회원가입 완료");
-
     try {
-      await postSignUp({ email, password });
+      await postSignUp(info);
+      alert("정삭적으로 회원가입 완료!");
       navigate("/signin");
     } catch (error) {
-      alert("이미 중복된 이메일입니다.");
+      alert("aaa");
     }
 
+    setInfo({ email: "", password: "" });
     setIsEmail(false);
     setIsPassword(false);
-    setEmail("");
-    setPassword("");
   };
 
   return (
@@ -58,7 +48,7 @@ function SignUp() {
         <input
           data-testid="email-input"
           placeholder="email"
-          value={email}
+          value={info.email}
           onChange={onChangeEmail}
         />
         {!isEmail ? <span>이메일 형식 X</span> : undefined}
@@ -68,7 +58,7 @@ function SignUp() {
           data-testid="password-input"
           placeholder="password"
           type="password"
-          value={password}
+          value={info.password}
           onChange={onChangePassword}
         />
         {!isPassword ? <span>비밀번호 8 자리 이상</span> : undefined}
@@ -77,7 +67,7 @@ function SignUp() {
       <button
         data-testid="signup-button"
         onClick={onSubmit}
-        disabled={isEmail && isPassword ? false : true}
+        disabled={!(isEmail && isPassword)}
       >
         회원가입
       </button>
