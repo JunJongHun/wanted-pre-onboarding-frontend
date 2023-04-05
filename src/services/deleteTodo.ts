7e16dfceaf2f88a25ceeecb635b4b19ec3b54c5c
jonghun <minn4072@gmail.com>
2023-04-06 06:49:26 +0900

    feat: todo 삭제 api 구현

--- src/services/deleteTodo.ts
+++ src/services/deleteTodo.ts
@@ -1,6 +1,6 @@
 import axiosCustom from "./axiosCustom";
 
-export default async function updateTodo(id: number) {
+export default async function deleteTodo(id: number) {
   const accessToken = localStorage.getItem("jwt");
 
   return await axiosCustom.delete(`/todos/${id}`, {
