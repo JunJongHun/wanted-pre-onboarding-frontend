6acb503498ff5171c133f29d61faa450b67f9188
jonghun <minn4072@gmail.com>
2023-04-08 20:48:53 +0900

    refactor: 이미지 Layout Shift 방지 구현

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,11 +4,14 @@ function Main() {
   const navigate = useNavigate();
   return (
     <div className="mt-12 flex flex-col items-center justify-center">
-      <img
-        className=" max-w-screen-md"
-        src="/frontendInternship.jpeg"
-        alt="프리온보딩 이미지"
-      />
+      <div className="h-96 max-w-screen-md">
+        <img
+          className="h-full w-full object-cover"
+          src="/frontendInternship.jpeg"
+          alt="프리온보딩 이미지"
+        />
+      </div>
+
       <div className="flex">
         <button
           className="m-3  rounded-lg  bg-blue-600 p-3 text-white  hover:scale-110"
